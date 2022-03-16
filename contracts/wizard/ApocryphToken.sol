60b892de5fed027483f930b4ab5fd0dcd8a2abdb
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2022-03-16 21:10:41 +0200

    feat: Add Pausable to ApocryphToken

--- contracts/wizard/ApocryphToken.sol
+++ contracts/wizard/ApocryphToken.sol
@@ -2,13 +2,15 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 /// @custom:security-contact apocryph@comrade.coop
-contract ApocryphToken is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+contract ApocryphToken is Initializable, ERC20Upgradeable, ERC20PermitUpgradeable, AccessControlUpgradeable, UUPSUpgradeable, PausableUpgradeable {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
 
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -17,12 +19,30 @@ contract ApocryphToken is Initializable, ERC20Upgradeable, ERC20PermitUpgradeabl
     function initialize(address owner) initializer public {
         __ERC20_init("Apocryph Token", "CRYPH");
         __ERC20Permit_init("Apocryph Token");
+        __Pausable_init();
         __AccessControl_init();
         __UUPSUpgradeable_init();
 
         _mint(owner, 1000000000 * 10 ** decimals());
         _grantRole(DEFAULT_ADMIN_ROLE, owner);
+        _grantRole(PAUSER_ROLE, owner);
         _grantRole(UPGRADER_ROLE, owner);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 
     function _authorizeUpgrade(address newImplementation)
--- deploy/00_apo_token.js
+++ deploy/00_apo_token.js
@@ -2,24 +2,33 @@ module.exports = async () => {
     const {deployer, comradeBoard: updater} = await getNamedAccounts()
     const ApocryphToken = await ethers.getContractFactory('ApocryphToken', await ethers.getSigner(deployer))
 
-    let proxy = await deployments.getOrNull('ApocryphToken')
+    let proxyAddress
 
+    let proxy = await deployments.getOrNull('ApocryphToken')
     if (proxy) {
       //await deployments.catchUnknownSigner(upgrades.upgradeProxy(proxy, ApocryphToken))
 
       const implAddress = await upgrades.prepareUpgrade(proxy.address, ApocryphToken)
-      await deployments.catchUnknownSigner(deployments.execute('ApocryphToken', {from: updater}, 'upgradeTo', implAddress))
-      console.log('Update ApocryphToken Impl  done ->', proxy.address, implAddress)
+      const currentAddress = await upgrades.erc1967.getImplementationAddress(proxy.address)
+
+      if (implAddress != currentAddress) {
+        await deployments.catchUnknownSigner(deployments.execute('ApocryphToken', {from: updater}, 'upgradeTo', implAddress))
+        console.log('Update ApocryphToken Impl  done ->', proxy.address, implAddress)
+      } else {
+        console.log('Up to date ApocryphToken Impl   --')
+      }
+      proxyAddress = proxy.address
     } else {
       const proxyContract = await upgrades.deployProxy(ApocryphToken, [updater], {kind: 'uups'})
       await proxyContract.deployTransaction.wait()
-      proxy = {
-        address: proxyContract.address,
-        ...await deployments.getExtendedArtifact('ApocryphToken')
-      }
-      await deployments.save('ApocryphToken', proxy)
       console.log('Deploy ApocryphToken Proxy done -> ', proxyContract.address)
+      proxyAddress = proxyContract.address
     }
+
+    await deployments.save('ApocryphToken', {
+      address: proxyAddress,
+      ...await deployments.getExtendedArtifact('ApocryphToken')
+    })
 }
 
 module.exports.tags = ['base', 'ApocryphToken']
